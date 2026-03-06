3a9c858a9548ec8500e74270586dd3d0a187ea78
Kusekushi <0kusekushi0@gmail.com>
2026-03-06 09:51:55 +0200

    Actually preprocess the tailwind source

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router-dom'
-import './App.css'
 import LoginPage from './pages/Login'
 import ResetPassword from './pages/ResetPassword'
 import { ProtectedRoute } from './components/ProtectedRoute'
